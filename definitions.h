500e519b389088d2b5316b37110559ddcb4c988c
JULIO NEUMAN KESSEL <e1511745@ibicui.grad.inf.puc-rio.br>
2017-11-23 16:48:22 -0200

    Added index to PageFrame

--- definitions.h
+++ definitions.h
@@ -1,17 +1,19 @@
-typedef struct PageFrame{
-    int count;
-    Page a;
-} PageFrame;
-
-typedef struct Page{
+typedef struct page{
     int index;
     // The atributes below refer to the current access to the page
     int proc_number;
     int offset;
     char type; // 'r' or 'w'
+    int bitM;
 } Page;
 
-typedef struct PageTableElement{
-    Page page;
-    PageFrame frame;
+typedef struct pageFrame{
+    int count;
+    int index;
+    Page *a;
+} PageFrame;
+
+typedef struct pageTableElement{
+    Page *page;
+    PageFrame *frame;
 } PageTableElement;
