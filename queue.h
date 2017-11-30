0568ab7a3f1a1774d8fc48567ae4cd2f0907eb3b
Pedro Sousa Meireles <psmeireles25@gmail.com>
2017-11-30 00:30:43 -0200

    Removed queue

--- queue.h
+++
@@ -1,8 +0,0 @@
-typedef struct queue Queue;
-typedef struct node Node;
-
-Queue* queue_create ();
-void queue_push (Queue* q, void* obj);
-void * queue_pop (Queue* q);
-int queue_isempty (Queue* q);
-void queue_free (Queue* q); 
